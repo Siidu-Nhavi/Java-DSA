a084e9c39e1aa4904ef9ef6f232c08a44f382712
Siddu Nhavi <siddunhavi@gmail.com>
2026-03-20 16:45:48 +0530

    applied the binary search to optimize the solution

--- PriorityQueue/WeakestSoliders.java
+++ PriorityQueue/WeakestSoliders.java
@@ -20,22 +20,32 @@ public class WeakestSoliders {
         }
     }
 
+    private static int binarySearch(int arr[]) {
+        int left = 0, right = arr.length - 1;
+        while (left <= right) {
+            int mid = (left + right) / 2;
+            if (arr[mid] == 1) {
+                left = mid + 1; // move right to find more 1's
+            } else {
+                right = mid - 1; // move left to find the more 1's
+            }
+        }
+        return left; // left will be the count of 1's in the row
+    }
+
     public static void main(String args[]) {
         int arr[][] = { { 1, 0, 0, 0 }, { 1, 1, 1, 1 }, { 1, 0, 0, 0 }, { 1, 0, 0, 0 } };
         int k = 2;
 
         PriorityQueue<Row> pq = new PriorityQueue<>();
 
-        for(int i=0;i<arr.length;i++){
-            int count = 0;
-            for(int j=0;j<arr[0].length;j++){
-                count +=  arr[i][j]==1?1:0;
-            }
+        for (int i = 0; i < arr.length; i++) {
+            int count = binarySearch(arr[i]);
             pq.add(new Row(count, i));
         }
 
-        for(int i=0;i<k;i++){
-            System.out.println("R"+pq.remove().idx); // poll and remove are same in priority queue oprations
+        for (int i = 0; i < k; i++) {
+            System.out.println("R" + pq.remove().idx); // poll and remove are same in priority queue oprations
         }
 
     }
