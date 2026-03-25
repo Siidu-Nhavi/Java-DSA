b47277c2c8c43ab8d72470b78ce83218448a669a
Siddu Nhavi <siddunhavi@gmail.com>
2026-03-25 19:13:40 +0530

    implemented the correct logic

--- STACK/MaxAreaHistogram.java
+++ STACK/MaxAreaHistogram.java
@@ -2,25 +2,46 @@ import java.util.*;
 public class MaxAreaHistogram {
     public static void maxArea(int [] arr){
          Stack<Integer> s = new Stack<>();
-        // int maxarea = 0;
-        // int nsr[] = new int[arr.length];
-        int nsl[] = new int[arr.length];
+         int maxarea = 0;
+         int nsr[] = new int[arr.length];
+         int nsl[] = new int[arr.length];
 
       
         //next smaller left
         for(int i =0; i<arr.length;i++){
             //while loop
-            while(s.isEmpty() && arr[s.peek()] <= arr[i]){
+            while(!s.isEmpty() && arr[s.peek()] >= arr[i]){
                 s.pop();
             }
             if(s.isEmpty()){
                 nsl[i] = -1;
             }else{
                 nsl[i] = s.peek();
-                s.push(i);
             }
-            
+            s.push(i);
         }
+
+        //next smaller right
+        s = new Stack<>();
+        for(int i =arr.length-1; i>=0;i--){
+            while(!s.isEmpty() && arr[s.peek()] >= arr[i]){
+                s.pop();
+            }
+            if(s.isEmpty()){
+                nsr[i] = arr.length;
+            }else{
+                nsr[i] = s.peek();
+            }
+            s.push(i);
+        }
+
+        //calculate max area
+        for(int i=0; i<arr.length; i++){
+            int width = nsr[i] - nsl[i] - 1;
+            int area = arr[i] * width;
+            maxarea = Math.max(maxarea, area);
+        }
+        System.out.println("Max Area: " + maxarea);
        
     }
     public static void main(String args[]){
