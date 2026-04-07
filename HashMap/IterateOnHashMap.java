594899c49c6d2f549e646ad4dbc9a56b313a86d1
Siddu Nhavi <siddunhavi@gmail.com>
2026-04-07 23:16:46 +0530

    Iteration the map

---
+++ HashMap/IterateOnHashMap.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+public class IterateOnHashMap {
+    public static void main(String args[]){
+        HashMap<String, Integer> map = new HashMap<>();
+        map.put("A",30);
+        map.put("B",90);
+        map.put("C",30);
+        map.put("D",50);
+        map.put("E",80);
+        map.put("F",90);
+
+        //iterate on the map
+        Set<String> k = map.keySet();
+        System.out.println(k);
+
+        //forEach to print the record
+          for (String str : k) {
+            System.out.println("Key="+str+",Value="+map.get(str));
+          }
+    }
+}
