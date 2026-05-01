a2241e4948c001b155300aa3a85cf8601c129f18
Siddu Nhavi <siddunhavi@gmail.com>
2026-05-01 23:57:42 +0530

    Solved Duplicate finding ex

---
+++ HashMap/ContainsDuplicate.java
@@ -0,0 +1,16 @@
+public class ContainsDuplicate {
+    public static boolean containsDuplicate(int arr[]){
+        for(int i=0;i<arr.length;i++){
+            for(int j=i+1;j<arr.length;j++){
+                if(arr[i] == arr[j]){
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+    public static void main(String[] args) {
+        int arr[] = {1,2,3,4,5,6,7,8,9,1};
+         System.out.print(containsDuplicate(arr));
+    }
+}
