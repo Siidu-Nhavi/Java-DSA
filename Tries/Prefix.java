b46a99620765d5c6e777e74ee7d67cb794345106
Siddu Nhavi <siddunhavi@gmail.com>
2026-06-16 15:38:45 +0530

    Solved the finding prefix in the string using tries datastructure

---
+++ Tries/Prefix.java
@@ -0,0 +1,59 @@
+public class Prefix {
+    static class Node {
+        Node[] childern = new Node[26];
+        boolean eow = false;
+        int freq;
+
+        public Node() {
+            for (int i = 0; i < childern.length; i++) {
+                childern[i] = null;
+            }
+            freq = 1;
+        }
+    }
+
+    public static Node root = new Node();
+
+    public static void insert(String word) {
+        Node curr = root;
+        for (int i = 0; i < word.length(); i++) {
+            int idx = word.charAt(i) - 'a';
+            if (curr.childern[idx] == null) {
+                curr.childern[idx] = new Node();
+            } else {
+                curr.childern[idx].freq++;
+            }
+            curr = curr.childern[idx];
+        }
+        curr.eow = true;
+    }
+
+    public static void findPrefix(Node root, String ans) {
+
+        if (root == null) {
+            return;
+        }
+        if (root.freq == 1) {
+            System.out.println(ans);
+            return;
+        }
+        for (int i = 0; i < root.childern.length; i++) {
+            if (root.childern[i] != null) {
+                findPrefix(root.childern[i], ans + (char) (i + 'a'));
+            }
+        }
+    }
+
+    public static void main(String args[]) {
+        String[] words = { "dog", "duck", "dove", "zebra" };
+
+        for (int i = 0; i < words.length; i++) {
+            insert(words[i]);
+        }
+
+        root.freq = -1;
+
+        System.out.println("All prefix from the string are=");
+        findPrefix(root, "");
+    }
+}
