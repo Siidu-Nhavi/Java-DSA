25ed94f72a41cfd6705294799944ed5cafa8f70c
Siddu Nhavi <siddunhavi@gmail.com>
2026-06-18 21:42:43 +0530

    Modified the method names

--- HashMap/CountDistinct.java
+++ HashMap/CountDistinct.java
@@ -1,32 +1,37 @@
 import java.util.*;
+
 public class CountDistinct {
-    public static int countDistinct(int arr[]){
+
+    // Method 1: Brute force approach
+    public static int countDistinctBrute(int arr[]) {
         int res = 0;
-        for(int i=0;i<arr.length;i++){
+        for (int i = 0; i < arr.length; i++) {
             boolean isDuplicate = false;
-            for(int j=0;j<i;j++){
-                if(arr[i] == arr[j]){
+            for (int j = 0; j < i; j++) {
+                if (arr[i] == arr[j]) { // check if arr[i] is present before it
                     isDuplicate = true;
                     break;
                 }
             }
-            if(!isDuplicate){
+            if (!isDuplicate) {
                 res++;
             }
         }
         return res;
     }
-    // by hashset
-    public static int countDistict(int arr[]){
+
+    // Method 2: Using HashSet
+    public static int countDistinctHashSet(int arr[]) {
         HashSet<Integer> s = new HashSet<>();
-        for(int num: arr){
+        for (int num : arr) {
             s.add(num);
         }
         return s.size();
     }
-    public static void main(String args[]){
-        int arr[] = {4,3,2,5,6,7,3,4,2,1};
-        // System.out.println("The no of distict elements are="+countDistinct(arr));
-        System.out.println("The no of distict elements are="+countDistict(arr));
+
+    public static void main(String args[]) {
+        int arr[] = { 4, 3, 2, 5, 6, 7, 3, 4, 2, 1 };
+        System.out.println("Distinct count (Brute) = " + countDistinctBrute(arr));
+        System.out.println("Distinct count (HashSet) = " + countDistinctHashSet(arr));
     }
 }
---
+++ HashMap/tempCodeRunnerFile.java
@@ -0,0 +1,1 @@
+ // System.out.println("The no of distinct elements are="+countDistict(arr));
